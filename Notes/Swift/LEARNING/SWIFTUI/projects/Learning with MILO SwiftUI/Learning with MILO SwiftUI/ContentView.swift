d0249af57c047737150538d70219d45334e3733a
Emilio Cruz Vargas <emilio.cruz.vargas123@gmail.com>
2025-08-19 17:42:42 -0600

    feat(GIT-0006): Adding a new project with the special implentation of CoreData

---
+++ Notes/Swift/LEARNING/SWIFTUI/projects/Learning with MILO SwiftUI/Learning with MILO SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Learning with MILO SwiftUI
+//
+//  Created by Emilio Cruz Vargas on 18/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
